9bb5dd748e457888ed324168f105461f49c84f6f
Abid Talukdar <abiditalukdar@gmail.com>
2020-01-14 01:50:51 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,16 +1,6 @@
 def square_array(array)
   # your code here
   new_numbers = []
-  while i < array.length do 
-    new_numbers.push(array[i]**2)
-    i++
-  end   
-end
-
-
-def square_array(array)
-  # your code here
-  new_numbers = []
   i = 0
   while i < array.length do 
     new_numbers.push(array[i]**2)
